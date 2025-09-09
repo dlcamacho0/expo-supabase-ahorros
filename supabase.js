661c453f64734941bfd8a752e2ea2bb6887be998
Deiby <deileo5504@gmail.com>
2025-09-09 17:07:41 -0500

    Quito supabase.js del repositorio

--- supabase.js
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://vyihbujkbsyltodxgusq.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
